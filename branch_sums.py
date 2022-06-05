11ebe6e67dd1b190df80f73acb1fe6010d72322f
Jacob Meyer <jacobpmeyer87@gmail.com>
2022-06-04 19:57:13 -0500

    Solve with recursion

---
+++ branch_sums.py
@@ -0,0 +1,34 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+# Add self to current total
+# if left and right are None, push current total to totals array and return
+# if left or right aren't empty, then call traverseLeftAndRight with:
+#       runningTotal, branchTotals, and whichever child isn't nil.
+
+
+def branchSums(root):
+    currentNode = root
+    branchTotals = traverseLeftAndRight(tree, 0, [])
+
+    return branchTotals
+
+def traverseLeftAndRight(node, runningTotal, branchTotals):
+    newTotal = runningTotal + node.value
+
+    if node.left is not None:
+        traverseLeftAndRight(node.left, newTotal, branchTotals)
+
+    if node.right is not None:
+        traverseLeftAndRight(node.right, newTotal, branchTotals)
+
+    if node.left is None and node.right is None:
+        branchTotals.append(newTotal)
+
+    return branchTotals
+
