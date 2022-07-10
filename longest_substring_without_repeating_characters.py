dee242a01224010ecf56d369ecf2e194120f047a
Jacob Meyer <jacobpmeyer87@gmail.com>
2022-07-10 17:44:46 -0500

    Make test cases for longest_substring

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,53 +1,50 @@
+import unittest
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        keys = {}
-        for i, char in enumerate(s):
-            if keys.get(char) is None:
-                keys[char] = []
-                keys[i] = []
-            keys[char].append(i)
-            firstIdx = keys[char][0]
-            keys[firstIdx].append(i)
+        n = len(s)
+        charPositions = {}
+        longest = 0
+        start = 0
+        for end in range(n):
+            char = s[end]
+            if char in charPositions:
+                start = max(start, charPositions[char])
 
-        longest = 1
-        for key, arr in keys.items():
-            arr.append(len(s) - 1)
-            if type(key) == str or len(arr) == 1:
-                next
-            i = 0
-            while i < len(arr) - 1:
-                unbroken = True
-                for j in range(arr[i], arr[i + 1]):
-                    # can't have more than one apearance between the two being checked
-                    startCheck, endCheck = arr[i], arr[i + 1]
-                    if keys.get(j) is not None:
-                        count = 1
-                        for pos in keys[j]:
-                            if pos > startCheck and pos < endCheck:
-                                count += 1
-                        if count > 1:
-                            unbroken = False
-                            break
-
-                if unbroken is True:
-                    currentRun = arr[i + 1] - arr[i] + 1
-                    if currentRun > longest:
-                        if arr[i + 1] == len(s) - 1 and s[arr[i]] != s[arr[i + 1]]:
-                            print(arr[i], arr[i + 1])
-                            currentRun += 1
-                        # print(arr[i], arr[i + 1])
-                        longest = currentRun
-
-                i += 1
-
+            longest = max(longest, end - start + 1)
+            charPositions[char] = end + 1
         return longest
 
-string1 = "pwwkew"
-string2 = "abcabcbb"
-string3 = "bbbbbb"
-string4 = "dvdf"
-s = Solution()
-print(s.lengthOfLongestSubstring(string1)) #=> 3
-print(s.lengthOfLongestSubstring(string2)) #=> 3
-print(s.lengthOfLongestSubstring(string3)) #=> 1
-print(s.lengthOfLongestSubstring(string4)) #=> 3
+
+
+class TestSolution(unittest.TestCase):
+    def test_string1(self):
+        string1 = "pwwkew"
+        s = Solution()
+
+        self.assertEqual(s.lengthOfLongestSubstring(string1), 3, "Should be 3")
+
+
+    def test_string2(self):
+        string2 = "abcabcbb"
+        s = Solution()
+
+        self.assertEqual(s.lengthOfLongestSubstring(string2), 3, "Should be 3")
+
+
+    def test_string3(self):
+        string3 = "bbbbbb"
+        s = Solution()
+
+        self.assertEqual(s.lengthOfLongestSubstring(string3), 1, "Should be 1")
+
+
+    def test_string4(self):
+        string4 = "dvdf"
+        s = Solution()
+
+        self.assertEqual(s.lengthOfLongestSubstring(string4), 3, "Should be 3")
+
+
+if __name__ == "__main__":
+    unittest.main()
