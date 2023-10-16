af4d126412b89419f988c164ea41b570b03acd0d
Jacob Meyer <jacobpmeyer87@gmail.com>
2023-10-16 15:54:37 -0500

    Add solution to merge overlapping intervals

--- merge_overlapping_intervals.py
+++ merge_overlapping_intervals.py
@@ -20,6 +20,40 @@ def mergeOverlappingIntervals(intervals):
   return condensedIntervals
 
 
+"""
+This solution passes.
+If you know your max value is going to be less than the length of the input array,
+this would be better, if not, the above is still better.
+"""
+def mergeOverlappingIntervals(intervals):
+    intDict = {}
+    maxInt = float('-inf')
+    minInt = float('inf')
+    for i in intervals:
+        intDict[i[0]] = i[1]
+        if i[0] > maxInt:
+            maxInt = i[0]
+        if i[0] < minInt:
+            minInt = i[0]
+    i = minInt
+    while i <= maxInt:
+        if i not in intDict:
+            i += 1
+            continue
+        nextMax = intDict[i]
+        j = i + 1
+        while j <= nextMax:
+            if j in intDict:
+                if nextMax <= intDict[j]:
+                    intDict[i] = intDict[j]
+                    nextMax = intDict[j]
+                del intDict[j]
+            j += 1
+        i += 1
+    output = []
+    for key, value in intDict.items():
+        output.append([key, value])
+    return output
 
 
 
