f498a572e0d4cc07a2c85a64b5e63d28313615af
Jacob Meyer <jacobpmeyer87@gmail.com>
2023-10-16 15:13:43 -0500

    Update solution for array of products

--- array_of_products.py
+++ array_of_products.py
@@ -1,44 +1,19 @@
 def arrayOfProducts(array):
-  zeroes = numOfZeroes(array)
-  prod = getProduct(array)
-  if zeroes > 1:
-    return [0 for i in range(len(array))]
-  elif zeroes is 1:
-    return handleOneZero(array, prod)
-  else:
-    return handleNoZeroes(array, prod)
+    # [5, 1, 4, 2]
+    # [1, 5, 5, 20]
+    # [8, 8, 2, 1]
 
+    leftProducts = [1 for i in range(len(array))]
+    rightProducts = [1 for i in range(len(array))]
+    products = [1 for i in range(len(array))]
 
-def numOfZeroes(array):
-  count = 0
-  for num in array:
-    if num == 0:
-      count += 1
+    for i in range(1, len(array)):
+        leftProducts[i] = array[i - 1] * leftProducts[i - 1]
 
-  return count
+    for j in reversed(range(len(array) - 1)):
+        rightProducts[j] = array[j + 1] * rightProducts[j + 1]
 
+    for i in range(len(array)):
+        products[i] = leftProducts[i] * rightProducts[i]
 
-def getProduct(array):
-  prod = 1
-  for num in array:
-    if num != 0:
-      prod *= num
-
-  return prod
-
-
-def handleOneZero(array, prod):
-  for i in range(len(array)):
-    if array[i] is 0:
-      array[i] = prod
-    else:
-      array[i] = 0
-
-  return array
-
-
-def handleNoZeroes(array, prod):
-  for i in range(len(array)):
-    array[i] = prod // array[i]
-
-  return array
+    return products
