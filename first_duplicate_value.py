7570f20130564332dcc57aa5d5c27ea983b4c7a3
Jacob Meyer <jacobpmeyer87@gmail.com>
2023-10-16 15:21:39 -0500

    Update solution to first duplicate value

--- first_duplicate_value.py
+++ first_duplicate_value.py
@@ -1,18 +1,9 @@
 def firstDuplicateValue(array):
-  seenNums = {}
-  i = 0
-  while i < len(array):
-    if seenNums.get(array[i]) is True:
-      return array[i]
-    else:
-      seenNums[array[i]] = True
-    i += 1
+    dups = {}
+    for i in array:
+        if i in dups:
+            return i
+        else:
+            dups[i] = True
 
-  return -1
-
-# Time: O(n)
-# Space: O(n)
-
-a = [2, 1, 5, 2, 3, 3, 4]
-
-b = [2, 1, 5, 3, 3, 2, 4]
+    return -1
