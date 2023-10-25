268e98f99b9cfddc6a9ab067fc1915ce9074a51d
Jacob Meyer <jacobpmeyer87@gmail.com>
2023-10-25 14:50:00 -0500

    Update first duplicate solution

--- first_duplicate_value.py
+++ first_duplicate_value.py
@@ -1,3 +1,4 @@
+# O(n) Time | O(n) Space
 def firstDuplicateValue(array):
     dups = {}
     for i in array:
@@ -6,4 +7,13 @@ def firstDuplicateValue(array):
         else:
             dups[i] = True
 
+    return -1
+
+# O(n) Time | O(1) Space
+def firstDuplicateValue(array):
+    for num in array:
+        absVal = abs(num)
+        if array[absVal - 1] < 0:
+            return absVal
+        array[absVal - 1] *= -1
     return -1
